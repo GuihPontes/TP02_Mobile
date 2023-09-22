49868b8c4b5d8ddd3b31482893f80a858f91356f
Guilherme Santos <guilherme-pontes@hotmail.com>
2023-09-22 18:38:51 -0300

    Adicionar arquivos de projeto.

---
+++ MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TP02;
+
+public static class MauiProgram
+{
+
+    //Breno e Guilherme
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+
+
+        return builder.Build();
+    }
+}
